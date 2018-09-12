4de1eb6bfa200e5ccc567918ff77b11aac11420f
Ben Linsay <blinsay@gmail.com>
2018-09-11 23:50:29 -0400

    update PROMPT

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -10,7 +10,7 @@ ls --color -d . &>/dev/null 2>&1 && \
 # prompt
 setopt prompt_subst
 
-PROMPT='[%*] %{$fg[cyan]%}%n%{$reset_color%}:%{$fg[green]%}%c%{$reset_color%}$(git_prompt_info) %(!.#.$) '
+PROMPT='[%*] %{$fg[cyan]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%} %{$fg[green]%}%2~%{$reset_color%}$(git_prompt_info) %(!.#.$) '
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[yellow]%}git:("
 ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
