64b6c48438ef2d6d041a8a59f02e3d365b216645
Ben Linsay <blinsay@gmail.com>
2025-02-03 14:49:56 -0500

    more

--- config.fish
+++ config.fish
@@ -1,9 +1,9 @@
-fish_add_path "/opt/homebrew/sbin"
-fish_add_path "/opt/homebrew/bin/"
+fish_add_path /opt/homebrew/sbin
+fish_add_path /opt/homebrew/bin/
+fish_add_path ~/go/bin
 
 for dot_dir in "$(dirname $(readlink -f ~/.config/fish/config.fish))"
-  for file in $dot_dir/fish/*
-    source $file
-  end
+    for file in $dot_dir/fish/*
+        source $file
+    end
 end
-
