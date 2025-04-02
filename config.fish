e285678a5d66b8bc0e312a2108b48bf6663d048f
Ben Linsay <blinsay@gmail.com>
2025-04-02 17:43:59 -0400

    fish: helix as default, disable greeting, orbstack

--- config.fish
+++ config.fish
@@ -1,9 +1,17 @@
+set fish_greeting
+
 fish_add_path /opt/homebrew/sbin
-fish_add_path /opt/homebrew/bin/
+fish_add_path /opt/homebrew/bin
 fish_add_path ~/go/bin
+
+export EDITOR=hx
 
 for dot_dir in "$(dirname $(readlink -f ~/.config/fish/config.fish))"
     for file in $dot_dir/fish/*
         source $file
     end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
