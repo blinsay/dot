b1a76647dfaedb493be4bcc0377c39ab1151b427
Ben Linsay <blinsay@gmail.com>
2025-07-29 16:15:59 -0400

    add bat as a manpager

--- config.fish
+++ config.fish
@@ -14,3 +14,11 @@ export EDITOR=hx
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init.fish 2>/dev/null || :
+
+if command -q batcat
+    alias bat batcat
+end
+
+if type -q bat
+    export MANPAGER="bat -l man -p"
+end
